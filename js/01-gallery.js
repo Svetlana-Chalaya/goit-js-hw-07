0e740b696fd9d28ea41fea5638810935c4c6fad6
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-11 18:39:55 +0300

    hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,27 +3,56 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const container = document.querySelector(".gallery");
+const galleryContainer = document.querySelector(".gallery");
+const cardsGallery = containerGalleryCardsItems(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
 
-container.addEventListener("click", onclick);
+function containerGalleryCardsItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onclick);
 
 function onclick(evt) {
-  console.log(evt);
+  evt.preventDefault();
+  if (!evt.target.classList.contains(".gallery__item")) {
+    return;
+  }
+
+  console.log(evt.target.dataset.source);
 }
+
+console.log(galleryItems);
+
+galleryContainer.addEventListener("Escape", offclick);
+
+function offclick(event) {
+  console.log(event);
+}
+
+document.addEventListener("keydown", function (offclick) {
+  if (offclick.key === "Escape") {
+    offclick.hideModal();
+  }
+});
 
 // import * as basicLightbox from "basiclightbox";
 
 // const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
+//     <img src=""${preview}" width="800" height="600">
 // `);
 
 // instance.show();
-
-// div.gallery;
-
-console.log(galleryItems);
