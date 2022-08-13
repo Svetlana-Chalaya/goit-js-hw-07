07d2e9459c5361b53507ab132d1a98b02704ae76
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-13 19:42:53 +0300

    script task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -24,6 +24,71 @@ function containerGalleryCardsItems(galleryItems) {
     .join("");
 }
 
+galleryContainer.addEventListener("click", onclick);
+function onclick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains(".gallery__item")) {
+    return;
+  }
+  console.log(evt.target.dataset.source);
+}
+console.log(galleryItems);
+
+galleryContainer.addEventListener("Escape", offclick);
+function offclick(event) {
+  console.log(event);
+}
+galleryContainer.addEventListener("click", offclick);
+
+function offclick(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="gallery__item" width="800" height="600">
+`);
+  instance.show();
+  console.log(event);
+}
+
+// const instance = basicLightbox.create(`
+//     <img src=""${preview}" width="800" height="600">
+// `);
+// instance.show();
+
+// galleryContainer.addEventListener("click", onclick);
+
+// function onclick(evt) {
+//   evt.preventDefault();
+//   if (!evt.target.classList.contains("gallery__item")) {
+//     return;
+//   }
+
+//   const resourceUrl = evt.target.dataset.source;
+//   console.log(resourceUrl);
+// }
+
+// galleryContainer.addEventListener("click", offclick);
+
+// function offclick(event) {
+//   event.preventDefault();
+//   const instance = basicLightbox.create(`
+//     <img src="gallery__item" width="800" height="600">
+// `);
+//   instance.show();
+//   console.log(event);
+// }
+
+// galleryContainer.addEventListener("click", onOffclick);
+
+// function onOffclick() {
+//   if (document.getElementById("styleSwitch").checked) {
+//     document.getElementById("gallery").classList.add("custom");
+//     document.getElementById("exampleModal").classList.add("custom");
+//   } else {
+//     document.getElementById("gallery").classList.remove("custom");
+//     document.getElementById("exampleModal").classList.remove("custom");
+//   }
+// }
+
 // galleryContainer.addEventListener("click", onclick);
 
 // function onclick(evt) {
